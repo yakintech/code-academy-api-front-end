777bc404cfa2b4525a1ec0b3a7dec6e71adc2381
cagatayyildiz <yildiz.cagatay@hotmail.com>
2023-06-02 16:23:57 +0300

    dotenv

--- config/db.js
+++ config/db.js
@@ -3,7 +3,6 @@ const { default: mongoose } = require("mongoose");
 
 const db = {
     connect: async () => {
-        console.log('process.env.CONNECTION',process.env.CONNECTION);
         try {
             await mongoose.connect(process.env.CONNECTION)
             console.log('CONNECTED!');
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { categoryController } = require('../controllers/categoryController');
+const { body } = require('express-validator');
+const { validate } = require('../middleware/Validation');
 
 const categoryRoutes = express.Router();
 
@@ -7,7 +9,15 @@ const categoryRoutes = express.Router();
 
 categoryRoutes.get('/', categoryController.getAll)
 categoryRoutes.get('/:id', categoryController.getById)
-categoryRoutes.post('/', categoryController.add)
+
+categoryRoutes.post(
+    '/',
+    body('name').notEmpty().withMessage('Name alanı boş geçilemez!'),
+    validate,
+    categoryController.add
+)
+
+
 categoryRoutes.delete('/:id', categoryController.deleteById)
 categoryRoutes.put('/:id', categoryController.update)
 
